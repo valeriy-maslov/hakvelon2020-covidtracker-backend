7662d6eeaeba0fcb534a6b390671e6a4086bbab9
Ksenya Maslova <ksenya-37@mail.ru>
2020-11-27 10:41:33 +0300

    Test

--- src/main/java/com/covidtracker/web/AccountAPI.java
+++ src/main/java/com/covidtracker/web/AccountAPI.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 
+/**
+ *
+ */
 @RestController
 @RequestMapping("/accounts")
 public class AccountAPI {
